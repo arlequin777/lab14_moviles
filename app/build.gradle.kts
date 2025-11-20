33e003da9afeb2b332c09ccb69a09eaeb0c1d186
jorgeabrilpino-hash <jorgeabril.pino@gmail.com>
2025-11-20 12:17:35 -0500

    "habilitar Jetpack Compose para soporte de Glance"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,14 +6,12 @@ plugins {
 
 android {
     namespace = "com.example.lab14"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.lab14"
         minSdk = 24
-        targetSdk = 36
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -29,17 +27,26 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
+    }
 }
+
+
 
 dependencies {
     implementation ("androidx.glance:glance-appwidget:1.1.0")
